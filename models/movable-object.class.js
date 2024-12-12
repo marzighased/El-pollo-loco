e63757cb453f0cfcd66e800dbf35fb66a28c0152
marzi ghased <marzighased@gmail.com>
2024-12-12 09:14:14 +0100

    added new js

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,10 +1,19 @@
 class MovableObject {
-    x;
-    y;
+    x = 120;
+    y = 400;
     img;
 
 
+    loadImage(path) {
+        this.img = new Image();  //this.img = document.getElementById('image') <img id="image">
+        this.img.src = path;
+    }
+
     moveRight() {
         console.log('Moving right');
+    }
+
+    moveLeft() {
+
     }
 }
