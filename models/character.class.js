d3dd41821efd3d8e3abedd7f9801256cde1be3bb
marzi ghased <marzighased@gmail.com>
2024-12-13 16:58:26 +0100

    character moves

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
 
     height = 280;
     y = 155;
+    speed = 10;
 
     IMAGES_WALKING = [
         'img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
@@ -11,7 +12,7 @@ class Character extends MovableObject {
         'img_pollo_locco/img/2_character_pepe/2_walk/W-25.png',
         'img_pollo_locco/img/2_character_pepe/2_walk/W-26.png' 
     ];
-    
+    world;
 
     constructor() {
         super().loadImage('img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
@@ -23,12 +24,29 @@ class Character extends MovableObject {
     animate() {
 
         setInterval(() => {
-        let i = this.currentImage % this.IMAGES_WALKING.length;   // let i = 0 % 6;
-        // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, ...
-        let path = this.IMAGES_WALKING[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
-        }, 100);
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            } 
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            } 
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT ) {
+
+               // Walk animation
+               let i = this.currentImage % this.IMAGES_WALKING.length;   // let i = 0 % 6;
+               // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, ...
+               let path = this.IMAGES_WALKING[i];
+               this.img = this.imageCache[path];
+               this.currentImage++;
+            }
+
+        }, 50);
     }
 
 
--- models/world.class.js
+++ models/world.class.js
@@ -21,11 +21,19 @@ class World {
    ];
    canvas;
    ctx;
+   keyboard;
 
-   constructor(canvas) {
+   constructor(canvas, keyboard) {
       this.ctx = canvas.getContext('2d');
       this.canvas = canvas;
+      this.keyboard = keyboard;
       this.draw();
+      this.setWorld();
+   }
+
+
+   setWorld() {
+      this.character.world = this;
    }
 
 
@@ -39,7 +47,7 @@ class World {
        this.addObjectsToMap(this.clouds);
 
 
-
+        // Draw wird immer wieder aufgerufen
         requestAnimationFrame(() => {
           this.draw();
      });
@@ -52,6 +60,16 @@ class World {
     }
   
     addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
