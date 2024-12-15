3e32dcf8398d55849804f61f5540a7ee29917a3d
marzi ghased <marzighased@gmail.com>
2024-12-15 15:59:14 +0100

    Gravitation and jump

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,11 +8,42 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
+
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+            this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 150;
+    }
 
 
     loadImage(path) {
         this.img = new Image();  //this.img = document.getElementById('image') <img id="image">
         this.img.src = path;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+
+        if (this instanceof Character || this instanceof Chicken) {
+             ctx.beginPath();
+             ctx.lineWidth = '5';
+             ctx.strokeStyle = 'blue';
+             ctx.rect(this.x, this.y, this.width, this.height);
+             ctx.stroke();
+        }
     }
 
     loadImages(arr) {
@@ -37,12 +68,14 @@ class MovableObject {
     }
 
     moveRight() {
-        console.log('Moving right');
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
+    }
+
+    jump() {
+        this.speedY = 30;
     }
 }
