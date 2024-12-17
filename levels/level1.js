a1ef88730cc1a410e4f6ceb89a7ea81c477a6dae
marzi ghased <marzighased@gmail.com>
2024-12-17 11:43:30 +0100

    initLevel added

--- levels/level1.js
+++ levels/level1.js
@@ -1,4 +1,7 @@
-const level1 = new Level(
+let level1;
+function initLevel() {
+
+level1 = new Level (
     [
         new Chicken(),
         new Chicken(),
@@ -11,7 +14,7 @@ const level1 = new Level(
     ],
 
     [
-        new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', -719),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', -719),
       new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/2.png', -719),
       new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/2.png', -719),
       new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/2.png', -719),
@@ -35,3 +38,4 @@ const level1 = new Level(
     ]
 
 );
+}
